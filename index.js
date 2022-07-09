91677c41dd2759a92ecb5625ff33e13b68967200
Hogtown13 <tribenation13@yahoo.com>
2022-07-08 21:18:33 -0400

    added view function prompts answers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import Employee from'./config/employee.js'
-import Department from'./config/department.js'
+import Employee from './config/employee.js'
+import Department from './config/department.js'
 import Role from './config/role.js'
 
 import mySql from 'mysql2'
@@ -8,28 +8,34 @@ import cTable from 'console.table'
 
 
 function view() {
-    prompt([{
+    prompt.prompt([{
         type: 'list',
         name: 'view',
-        message: 'What would you like to view?',
+        message: 'What would you like to do?',
         choices: [
-            'All Departments',
-            'All Roles',
-            'All Employees'
+            'view all Departments',
+            'view all Roles',
+            'view all Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee'
         ]
-    }]).then((answers) => {
-        console.log(answers)
-    })
-    // prompt([{
-    //     type: 'list',
-    //     name: 'add',
-    //     message: 'Would you like to add one of the following?',
-    //     choices: [
-    //         'Department',
-    //         'Role',
-    //         'Employee'
-    //     ]
-    // }])
+    }])
+        .then((answers) => {
+
+            if (answers.view == 'view all Departments') {
+                console.log('Fuck Yeah!!!')
+            } else {
+                console.log('finally')
+            }
+        })
+        .catch((err) => {
+            if (err) {
+                console.log('Something went wrong!')
+            } else {
+                console.log('Something else went wrong!')
+            }
+        });
 }
 
 view();
